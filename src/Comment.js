103c4c5243afdd3fa24dfab9658ac32d2c452883
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2023-10-29 22:49:57 +0300

    Completed assignment

--- src/Comment.js
+++ src/Comment.js
@@ -4,4 +4,11 @@ function Comment() {
   return <div>Naturally, I agree with this article.</div>;
 }
 
+// const Comment = () => <div>Naturally, I agree with this article.</div>;
+
+// class Comment extends React.Component {
+//   render() {
+//     return <div>Naturally, I agree with this article.</div>;
+//   }
+// }
 export default Comment;
